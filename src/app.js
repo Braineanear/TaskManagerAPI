52af8805be338c61588ed18511d50c2fdddb666e
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-20 11:56:34 +0200

    feat(init): server fils

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import compression from 'compression';
+import cors from 'cors';
+import mongoSanitize from 'express-mongo-sanitize';
+
+import config from './config/config';
+import { successHandle, errorHandle } from './config/morgan';
+
+import limiter from './middlewares/rateLimiter';
+
+import errorHandler from './utils/errorHandler';
+import AppError from './utils/appError';
+
+import routes from './routes/index';
+
+const app = express();
+
+app.enable('trust proxy');
+
+app.use(successHandle);
+app.use(errorHandle);
+
+app.use(helmet());
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(xss());
+
+app.use(mongoSanitize());
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(compression());
+
+app.disable('x-powered-by');
+
+if (config.env === 'production') {
+  app.use('/', limiter);
+}
+
+app.use('/', routes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorHandler);
+
+export default app;
