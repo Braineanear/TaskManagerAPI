934d0dae567eae4c99ec3f99e3389296336a0a38
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-10-20 11:54:22 +0200

    feat(config): add env configs

---
+++ src/config/config.js
@@ -0,0 +1,34 @@
+import dotenv from 'dotenv';
+import Joi from 'joi';
+
+dotenv.config({ path: '.env' });
+
+const envVarsSchema = Joi.object()
+  .keys({
+    NODE_ENV: Joi.string()
+      .valid('production', 'development', 'test')
+      .required(),
+    PORT: Joi.number().default(3000),
+    DATABASE_CONNECTION: Joi.string().required().description('MongoDB URL'),
+    DATABASE_PASSWORD: Joi.string().required().description('MongoDB Password')
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: 'key' } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const config = {
+  env: envVars.NODE_ENV,
+  port: envVars.PORT,
+  db: {
+    url: envVars.DATABASE_CONNECTION,
+    pass: envVars.DATABASE_PASSWORD
+  }
+};
+
+export default config;
